89c1518819f5eadb083f9ee2fef8ff13d4e4e01f
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2022-02-01 20:56:49 -0300

    JWT Authentication

--- src/user/auth/auth.module.ts
+++ src/user/auth/auth.module.ts
@@ -2,9 +2,20 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { Module } from '@nestjs/common';
 import { UserModule } from '../user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+          expiresIn: process.env.JWT_EXPIRE,
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
